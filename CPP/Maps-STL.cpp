8573891320d8de67555e32923df5f460213cabd1
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-01 17:07:50 +0530

    Implement map operations for student marks

--- CPP/Maps-STL.cpp
+++ CPP/Maps-STL.cpp
@@ -1,1 +1,41 @@
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int q ;
+    cin >> q ;
+    map<string , int > m ;
+    for(int i = 0 ; i< q ;++i){
+        int type ;
+        string name ;
+        cin >> type >> name ;
+        
+        if(type == 1 ){
+            int marks ; 
+            cin >> marks ;
+            m[name] +=marks ;
+        }else if (type == 2){
+            m.erase(name);
+        }else if (type == 3 ){
+            cout << m[name] << endl;
+        }
+        
+        
+        
+    }
+    
+    return 0;
+}
+
+
+
