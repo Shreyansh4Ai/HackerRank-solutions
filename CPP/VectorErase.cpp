bc0ca8b84dc1885fe6bffbfd2f4dc4074855a6bd
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-01 17:08:37 +0530

    Implement vector element erasure in C++

--- CPP/VectorErase.cpp
+++ CPP/VectorErase.cpp
@@ -1,1 +1,39 @@
-/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    
+    int n;
+    cin >> n ;
+    
+    vector<int>a;
+    for(int i =0 ; i<n ; i++ ){
+        int d ;
+        cin >> d ;
+        a.push_back(d);
+    }
+    
+    int x ;
+    cin >> x ;
+    
+    a.erase(a.begin() + (x-1));
+    
+    int z , y ;
+    cin >>  z >> y ;
+    
+    a.erase(a.begin()+(z-1) , a.begin()+(y-1));
+    
+    cout <<a.size()<<endl;
+    for(int i =0 ; i<a.size() ; i++){
+        
+        cout <<a[i]<<" ";
+    }
+       
+    return 0;
+}
