60e550639b1e4b2d699325950ebe79baaea226cc
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-11-01 17:09:34 +0530

    Implement vector sorting in vectorSort.cpp

--- CPP/vectorSort.cpp
+++ CPP/vectorSort.cpp
@@ -1,1 +1,29 @@
-..
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int n ;
+    cin >> n ;
+    
+    vector<int> a ;
+    for(int i =0 ; i<n ;i++){
+        int v ;
+        cin >> v;
+        a.push_back(v);
+    }
+    
+    sort(a.begin(),a.end());
+    
+    for(int i =0 ; i<n ;i++){
+        cout << a[i]<<" ";
+    }
+    
+    return 0;
+}
